3f7a0fcc40185b05dafd2e1a91ee0ce3044e863a
ptnkjke@gmail.com <ptnkjke@gmail.com>
2017-09-30 21:29:50 +0300

    deleting child project and move on top.

--- src/main/java/net/ptnkjke/jbeditor/MissingInstruction.java
+++ src/main/java/net/ptnkjke/jbeditor/MissingInstruction.java
@@ -1,8 +1,6 @@
 package net.ptnkjke.jbeditor;
 
-/**
- * Created by Lopatin on 09.07.2014.
- */
+
 public class MissingInstruction extends Exception {
     public MissingInstruction(String msg) {
         super(msg);
